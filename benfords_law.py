bdb5ca62a60c57488b4c16fa0255506da7387acb
Tom1432006 <74423596+Tom1432006@users.noreply.github.com>
2023-10-20 11:13:31 +0200

    amount

--- benfords_law.py
+++ benfords_law.py
@@ -4,10 +4,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-benfords_law = [30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6]
-benfords_law_second = [11.97, 11.39, 10.88, 10.43, 10.03, 9.67, 9.34, 9.04, 8.76, 8.50]
+def get_probability(x, digit):
+    if digit == 1:
+        return np.log10(1+1/x)*100
 
-def calculate(filename, start_row = 1, collumn = 1, end_row = None, last_digit = False, digit=1):
+    # this formula only works for digits grater than 1
+    # variables for the formula
+    lower = int(math.pow(10, digit-2))
+    upper = int(math.pow(10, digit-1)-1)
+
+    return sum(np.log10(1+(1/(10*k+x)))*100 for k in range(lower,  upper+1))
+
+def calculate(filename, start_row = 1, collumn = 1, end_row = None, last_digit = False, digit=1, amount=1):
     # load the data 
     data = []
     with open("data/" + filename + ".csv") as csvF:
@@ -16,8 +24,10 @@ def calculate(filename, start_row = 1, collumn = 1, end_row = None, last_digit =
             if row != []: data.append(row)
 
     # save the distribution
-    d = [0 for _ in range(9)]
-    if digit != 1 or last_digit: d.append(0)
+    min = 0
+    if digit == 1 and amount == 1 and not last_digit: min += 1
+    max = int(math.pow(10, amount))
+    d = [0 for _ in range(min, max)]
 
     # get the n digit
     nono_chars = ["-", "", "NA", "0"]
@@ -28,25 +38,31 @@ def calculate(filename, start_row = 1, collumn = 1, end_row = None, last_digit =
 
         if i >= start_row and row[collumn] not in nono_chars:
             # prepare the number to read the digits
-            number = row[collumn].replace(",", "") # remove kommas
-            number = row[collumn].replace(".", "") # remove dots
+            number = row[collumn]
+            number = number.replace(",", "") # remove kommas
+            number = number.replace(".", "") # remove dots
+            number.lstrip("0") # remove leading zeros
             number = int(number)
             
             # remove minus
             if number < 0: number *= -1
 
-            if last_digit:
-                d[int(str(number)[-1])] += 1
-            elif len(str(number)) > digit-1:
-                if digit == 1: d[int(str(number)[digit-1])-1] += 1
-                else:          d[int(str(number)[digit-1])] += 1
+            if amount > 1:
+                if number >= max:
+                    d[int(str(number)[digit-1:amount+(digit-1)])-1] += 1
+            else:
+                if last_digit:
+                    d[int(str(number)[-1])] += 1
+                elif len(str(number)) > digit-1:
+                    if digit == 1: d[int(str(number)[digit-1])-1] += 1
+                    else:          d[int(str(number)[digit-1])] += 1
 
         i += 1
         if end_row != None and i >= end_row: break
 
     # calculate percentage
     value_sum = sum(d)
-    perc = [0 for _ in range(9)]
+    perc = [0 for _ in range(min, max)]
     if digit != 1 or last_digit: perc.append(0)
 
     if value_sum == 0: exit("Es gibt dazu keine Daten")
@@ -67,6 +83,7 @@ if __name__ == "__main__":
     parser.add_argument("-l", "--last-digit", action='store_true')
     parser.add_argument("-p", "--plot-data", action='store_true')
     parser.add_argument("-t", "--digit", default=1)
+    parser.add_argument("-a", "--amount", default=1)
 
     parser.add_argument("-i", "--ignore-bendford", action='store_true')
 
@@ -74,47 +91,55 @@ if __name__ == "__main__":
 
     filename = args.data
     start_row = int(args.starting_row)
-    if args.ending_row == None:
-        ending_row = None
-    else:
-        ending_row = int(args.ending_row)
+    ending_row = None
+    if args.ending_row != None:  ending_row = int(args.ending_row)
     collumn = int(args.collumn)
     last_digit = args.last_digit
     plot_data = args.plot_data
     digit = int(args.digit)
+    amount = int(args.amount)
     ignore_bendford = args.ignore_bendford
 
     if last_digit: digit = 1
+    if last_digit: amount = 1
     #endregion
+    min = int(math.pow(10, amount-1))
+    if digit != 1 or last_digit: min = 0
+    max = int(math.pow(10, amount))
 
-    percentage, value_sum, spalte_name = calculate(filename, start_row, collumn, ending_row, last_digit, digit)
+    percentage, value_sum, spalte_name = calculate(filename, start_row, collumn, ending_row, last_digit, digit, amount)
 
     if spalte_name != "": print("Spalte: " + spalte_name)
 
-    for i in range(len(percentage)):
-        if digit == 2:                print(f"{i}: {percentage[i]}%  --- {benfords_law_second[i]}%")
-        elif digit > 2 or last_digit: print(f"{i}: {percentage[i]}%")
-        else:                         print(f"{i+1}: {percentage[i]}%  --- {benfords_law[i]}%")
+    # generate benfords law
+    benfords_law = []
+    start = 1 if digit == 1 else 0
+    for x in range(start, 10):
+        benfords_law.append(round(get_probability(x, digit), 2))
+
+    if amount == 1:
+        for i in range(len(percentage)-1):
+            if digit >= 2:   print(f"{i}: {percentage[i]}%  --- {benfords_law[i]}%")
+            elif last_digit: print(f"{i}: {percentage[i]}%")
+            else:            print(f"{i+1}: {percentage[i]}%  --- {benfords_law[i]}%")
 
     print("Errechnet aus " + str(value_sum) + " Datenwerten.")
 
     if plot_data:
         if digit == 1 and not last_digit:
-            x = np.linspace(1, 9, 100)
-            for i in range(1,10):
+            x = np.linspace(min, max-1, 100)
+            for i in range(min,max):
                 plt.bar(i, percentage[i-1], width=.8, color="#1f77b4")
         else:
-            x = [i for i in range(0, 10)]
-            for i in range(0,10):
+            x = np.linspace(min, max-1, 100)
+            for i in range(min,max):
                 plt.bar(i, percentage[i], width=.8, color="#1f77b4")
 
-        if not last_digit and not ignore_bendford and digit == 1:
-            plt.plot(x, np.log10(1+(1/x))*100, "r--")
-        elif not ignore_bendford and digit == 2:
-            plt.plot(x, benfords_law_second, "r--")
+        if (digit == 1 or amount == 1) and not ignore_bendford:
+            plt.plot(x, get_probability(x, digit), "r--")
 
         plt.xlabel('Ziffer')
-        plt.xticks([x for x in range(0,10)])
+        plt.xticks([x for x in range(min,max, math.ceil(max/20))])
         plt.ylabel('Prozent')
         if start_row > 0: plt.title(filename + " / " + spalte_name)
         else:             plt.title(filename)
---
+++ generator.py
@@ -0,0 +1,8 @@
+numbers = [25, 12]
+
+for i in range(2, 10000):
+    numbers.append(numbers[i-1] + numbers[i-2])
+
+with open("data/tom_numbers.csv", "w") as f:
+    for n in numbers:
+        f.write(str(n) + "\n")
